6742537bf311c80c3b92d87ee05a832db05d8b80
Chanithi Vinodya <bandarabvcv@gmail.com>
2026-06-22 23:53:47 +0530

    Create postRoutes.js

---
+++ backend/src/routes/postRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { getFeed, getPost, createPost, deletePost } = require('../controllers/postController');
+const { getComments, createComment } = require('../controllers/commentController');
+const { toggleLike } = require('../controllers/likeController');
+const { authenticate, optionalAuth } = require('../middleware/auth');
+const { upload } = require('../middleware/upload');
+
+router.get('/', authenticate, getFeed);
+router.post('/', authenticate, upload.single('image'), createPost);
+router.get('/:id', optionalAuth, getPost);
+router.delete('/:id', authenticate, deletePost);
+router.post('/:id/like', authenticate, toggleLike);
+router.get('/:id/comments', getComments);
+router.post('/:id/comments', authenticate, createComment);
+
+module.exports = router;
