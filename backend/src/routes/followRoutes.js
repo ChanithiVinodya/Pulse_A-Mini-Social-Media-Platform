c9ffee6e62cf7bbb8b781306689a4534f5d98303
Chanithi Vinodya <bandarabvcv@gmail.com>
2026-07-02 22:47:30 +0530

    Update followRoutes.js

--- backend/src/routes/followRoutes.js
+++ backend/src/routes/followRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const { toggleFollow } = require('../controllers/followController');
+const { toggleFollow, getFollowers, getFollowing } = require('../controllers/followController');
 const { authenticate } = require('../middleware/auth');
 
 router.post('/:id/follow', authenticate, toggleFollow);
+router.get('/:id/followers', authenticate, getFollowers);
+router.get('/:id/following', authenticate, getFollowing);
 
 module.exports = router;
