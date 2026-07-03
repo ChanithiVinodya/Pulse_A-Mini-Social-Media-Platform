b84b89002ced2847586b625b2e16f4186e9b5809
Chanithi Vinodya <bandarabvcv@gmail.com>
2026-07-03 13:45:04 +0530

    Update postRoutes.js

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getFeed, getPost, createPost, deletePost } = require('../controllers/postController');
+const { getFeed, getPost, createPost, deletePost, toggleSave } = require('../controllers/postController');
 const { getComments, createComment } = require('../controllers/commentController');
 const { toggleLike } = require('../controllers/likeController');
 const { authenticate, optionalAuth } = require('../middleware/auth');
@@ -9,6 +9,7 @@ router.get('/', authenticate, getFeed);
 router.post('/', authenticate, upload.single('image'), createPost);
 router.get('/:id', optionalAuth, getPost);
 router.delete('/:id', authenticate, deletePost);
+router.post('/:id/save', authenticate, toggleSave);
 router.post('/:id/like', authenticate, toggleLike);
 router.get('/:id/comments', getComments);
 router.post('/:id/comments', authenticate, createComment);
